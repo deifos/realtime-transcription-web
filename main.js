d5a2ae96a25afaf45d78c03f5eb951db4861d777
Vlad <vlad.palacio@gmail.com>
2025-02-10 15:57:52 -0500

    basic semi working version

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // In development, load from local server
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:3001");
+  } else {
+    // In production, load the built Next.js app
+    win.loadFile("out/index.html");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});

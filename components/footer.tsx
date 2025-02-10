c7d77bf97e99adb97da5e24a467f5d8876db143b
Vlad <vlad.palacio@gmail.com>
2025-02-10 14:39:05 -0500

    fixed typo

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-white py-4 mt-auto">
       <div className="container mx-auto text-center">
         <p className="text-slate-800">
-          Power Moonshine Web & by Transformers JS
+          Power by Moonshine Web & by Transformers JS
         </p>
       </div>
     </footer>

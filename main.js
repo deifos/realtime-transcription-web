f1dc0cc7cb9302b82af3368f6839bd60e67f4f3f
Vlad <vlad.palacio@gmail.com>
2025-02-10 16:07:59 -0500

    working version, needed to enable webgpu

--- main.js
+++ main.js
@@ -8,8 +8,14 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      webSecurity: true,
+      enableWebSQL: false,
     },
   });
+
+  // Enable WebGPU
+  app.commandLine.appendSwitch("enable-unsafe-webgpu");
+  app.commandLine.appendSwitch("enable-features", "Vulkan,UseSkiaRenderer");
 
   // In development, load from local server
   if (process.env.NODE_ENV === "development") {
@@ -18,7 +24,13 @@ function createWindow() {
     // In production, load the built Next.js app
     win.loadFile("out/index.html");
   }
+
+  win.webContents.openDevTools();
 }
+
+// This needs to be called before app is ready
+app.commandLine.appendSwitch("enable-unsafe-webgpu");
+app.commandLine.appendSwitch("enable-features", "Vulkan,UseSkiaRenderer");
 
 app.whenReady().then(createWindow);
 
